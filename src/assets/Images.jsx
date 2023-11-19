3c0959c5a34eeeadc07d8839417cf85ff263e4d4
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-19 21:29:29 +0300

    created Images comp for data imgs

---
+++ src/assets/Images.jsx
@@ -0,0 +1,14 @@
+/*Hero IMGS*/
+import summer from "./hero/hero-summer.jpeg";
+import winter from "./hero/hero-winter.jpg";
+
+/*RECOMENDED PRODUCTS*/
+import men from "./recomendedProducts/recomended-products-men.jpeg";
+import women from "./recomendedProducts/recomended-products-women.jpeg";
+import accessories from "./recomendedProducts/recomended-products-accessories.jpeg";
+import kids from "./recomendedProducts/recomended-products-kids.jpeg";
+
+export const Images = {
+    hero: { summer, winter },
+    recomendedProducts: { men, women, accessories, kids }
+}
