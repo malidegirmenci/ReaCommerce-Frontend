e18582c97c8b9a1b729ec4e5ff9fddd610e3f88d
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-19 21:45:29 +0300

    imported header and home comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Header from './layout/Header'
+import Home from './pages/Home';
 
 function App() {
-
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Header />
+      <Home/>
+    </>
   )
 }
 
