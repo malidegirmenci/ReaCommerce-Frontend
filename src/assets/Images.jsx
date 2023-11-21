57c7e57e0e791125240eed426c523cae38bf5de0
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-22 02:49:38 +0300

    changed imgs paths and imported bestseller imgs

--- src/assets/Images.jsx
+++ src/assets/Images.jsx
@@ -1,14 +1,26 @@
 /*Hero IMGS*/
-import summer from "./hero/hero-summer.jpeg";
-import winter from "./hero/hero-winter.jpg";
+import summer from "./hero/summer.jpeg";
+import winter from "./hero/winter.jpg";
 
-/*RECOMENDED PRODUCTS*/
-import men from "./recomendedProducts/recomended-products-men.jpeg";
-import women from "./recomendedProducts/recomended-products-women.jpeg";
-import accessories from "./recomendedProducts/recomended-products-accessories.jpeg";
-import kids from "./recomendedProducts/recomended-products-kids.jpeg";
+/*Categories*/
+import men from "./categories/men.jpeg";
+import women from "./categories/women.jpeg";
+import accessories from "./categories/accessories.jpeg";
+import kids from "./categories/kids.jpeg";
+
+/*Bestseller Products*/
+import product1 from "./bestSellerProducts/product-1.jpeg";
+import product2 from "./bestSellerProducts/product-2.jpeg";
+import product3 from "./bestSellerProducts/product-3.jpeg";
+import product4 from "./bestSellerProducts/product-4.jpeg";
+import product5 from "./bestSellerProducts/product-5.jpeg";
+import product6 from "./bestSellerProducts/product-6.jpeg";
+import product7 from "./bestSellerProducts/product-7.jpeg";
+import product8 from "./bestSellerProducts/product-8.jpeg";
 
 export const Images = {
     hero: { summer, winter },
-    recomendedProducts: { men, women, accessories, kids }
+    categories: { men, women, accessories, kids },
+    bestSellerProducts: {product1,product2,product3,product4,product5,product6,product7,product8,},
+    
 }
