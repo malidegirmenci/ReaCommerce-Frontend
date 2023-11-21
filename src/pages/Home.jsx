42c5da08f4ee6d73f581c0ae14744715293b31ef
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-22 02:53:07 +0300

    home page structure changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import Slider from '../components/Slider';
+import Slider from '../components/home/Slider';
 import { data } from '../data/data';
 import RecomendedProducts from '../components/home/RecomendedProducts';
+import BestSellerProducts from '../components/home/BestSellerProducts';
 
 export default function Home() {
     return (
         <>
-            <Slider data={data.home.hero}/>
-            <RecomendedProducts />
+            <div>
+                <Slider data={data.home.hero} />
+            </div>
+            <div className='flex flex-col justify-center w-full items-center gap-20'>
+                <RecomendedProducts />
+                <BestSellerProducts />
+            </div>
         </>
     )
 }
