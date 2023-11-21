1f44afc29bee5f9b59f848a7e7e6d33a42a332ab
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-22 02:51:41 +0300

    created and imported category and product card

---
+++ src/components/home/CategoryCard.jsx
@@ -0,0 +1,9 @@
+export default function CategoryCard(props) {
+    const {name, img} = props.data;
+    return (
+        <>
+            <img src={img} className='w-full h-full object-cover' />
+            <button className=' absolute bottom-[5%] left-[5%] w-44 text-slate-800 text-base font-bold leading-normal tracking-tight py-3 bg-white'>{name}</button>
+        </>
+    )
+}
---
+++ src/components/home/ProductCard.jsx
@@ -0,0 +1,24 @@
+export default function ProductCard(props) {
+    const { name, category, price, discountedPrice, colors, img } = props.data;
+    return (
+        <div className="flex flex-col text-center w-[20%]">
+            <div className="h-4/5 ">
+                <img src={img} className="max-w-full h-full object-cover" />
+            </div>
+            <div className="p-6 flex-col gap-2">
+                <h5 className="text-slate-800 text-base font-bold leading-normal tracking-tight">{name}</h5>
+                <h5 className="text-neutral-500 text-sm font-bold leading-normal tracking-tight">{category}</h5>
+                <div className="flex gap-1 justify-center">
+                    <h5 className="text-stone-300 text-base font-bold leading-normal tracking-tight">${price}</h5>
+                    <h5 className="text-teal-700 text-base font-bold leading-normal tracking-tight">${discountedPrice}</h5>
+                </div>
+                <div className="w-20 h-4 justify-start items-center gap-1.5 inline-flex">
+                    <div className="w-4 h-4 bg-sky-500 rounded-full" />
+                    <div className="w-4 h-4 bg-teal-700 rounded-full" />
+                    <div className="w-4 h-4 bg-orange-400 rounded-full" />
+                    <div className="w-4 h-4 bg-slate-800 rounded-full" />
+                </div>
+            </div>
+        </div>
+    )
+}
