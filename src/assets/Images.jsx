1abe2e9fa6f0c535262f4bdba002bb08b36fa72b
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-24 00:10:38 +0300

    added routing, folders renamed and completed home

--- src/assets/Images.jsx
+++ src/assets/Images.jsx
@@ -1,6 +1,6 @@
 /*Hero Top IMGS*/
-import summerHeroTop from "./heroTop/heroTop-summer.jpeg";
-import winterHeroTop from "./heroTop/heroTop-winter.jpg";
+import summerHeroTop from "./hero-top/heroTop-summer.jpeg";
+import winterHeroTop from "./hero-top/heroTop-winter.jpg";
 
 /*Categories*/
 import men from "./categories/men.jpeg";
@@ -9,26 +9,26 @@ import accessories from "./categories/accessories.jpeg";
 import kids from "./categories/kids.jpeg";
 
 /*Bestseller Products*/
-import product1 from "./bestSellerProducts/product-1.jpeg";
-import product2 from "./bestSellerProducts/product-2.jpeg";
-import product3 from "./bestSellerProducts/product-3.jpeg";
-import product4 from "./bestSellerProducts/product-4.jpeg";
-import product5 from "./bestSellerProducts/product-5.jpeg";
-import product6 from "./bestSellerProducts/product-6.jpeg";
-import product7 from "./bestSellerProducts/product-7.jpeg";
-import product8 from "./bestSellerProducts/product-8.jpeg";
+import product1 from "./bestseller-products/product-1.jpeg";
+import product2 from "./bestseller-products/product-2.jpeg";
+import product3 from "./bestseller-products/product-3.jpeg";
+import product4 from "./bestseller-products/product-4.jpeg";
+import product5 from "./bestseller-products/product-5.jpeg";
+import product6 from "./bestseller-products/product-6.jpeg";
+import product7 from "./bestseller-products/product-7.jpeg";
+import product8 from "./bestseller-products/product-8.jpeg";
 
 /*Hero Bot IMGS*/
-import summerHeroBot from "./heroBot/heroBot-summer.png";
-import winterHeroBot from "./heroBot/heroBot-winter.png";
+import summerHeroBot from "./hero-Bot/heroBot-summer.png";
+import winterHeroBot from "./hero-Bot/heroBot-winter.png";
 
 /*Popular Product*/
-import popularProduct from "./popularProduct/popular-product.png";
+import popularProduct from "./popular-product/popular-product.png";
 
 /*Featured Posts*/
-import post1 from "./featuredPosts/post-1.jpeg";
-import post2 from "./featuredPosts/post-2.jpeg";
-import post3 from "./featuredPosts/post-3.jpeg";
+import post1 from "./featured-posts/post-1.jpeg";
+import post2 from "./featured-posts/post-2.jpeg";
+import post3 from "./featured-posts/post-3.jpeg";
 
 export const Images = {
     heroTop: { summerHeroTop, winterHeroTop },
