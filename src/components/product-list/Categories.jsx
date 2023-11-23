154da778cf43511928a609201ae8eb2c27b9e244
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-24 02:29:08 +0300

    created categories and categorycard into it

---
+++ src/components/product-list/Categories.jsx
@@ -0,0 +1,13 @@
+import { data } from "../../data/data"
+import CategoryCard from "./CategoryCard";
+
+export default function Categories(){
+    const categories = data.productList.categories.map(item => {
+        return <CategoryCard data={item} key={item.key} />
+    })
+    return(
+        <div className="flex w-full ">
+            {categories}
+        </div>
+    );
+}
