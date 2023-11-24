2ba2776180d5fcf81e621b6a1a5f0a9e2300d422
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-24 04:52:27 +0300

    product imgs imported

--- src/assets/Images.jsx
+++ src/assets/Images.jsx
@@ -10,14 +10,14 @@ import accessories from "./home/categories/accessories.jpeg";
 import kids from "./home/categories/kids.jpeg";
 
 /*Bestseller Products*/
-import product1 from "./home/bestseller-products/product-1.jpeg";
-import product2 from "./home/bestseller-products/product-2.jpeg";
-import product3 from "./home/bestseller-products/product-3.jpeg";
-import product4 from "./home/bestseller-products/product-4.jpeg";
-import product5 from "./home/bestseller-products/product-5.jpeg";
-import product6 from "./home/bestseller-products/product-6.jpeg";
-import product7 from "./home/bestseller-products/product-7.jpeg";
-import product8 from "./home/bestseller-products/product-8.jpeg";
+import product1InBS from "./home/bestseller-products/product-1.jpeg";
+import product2InBS from "./home/bestseller-products/product-2.jpeg";
+import product3InBS from "./home/bestseller-products/product-3.jpeg";
+import product4InBS from "./home/bestseller-products/product-4.jpeg";
+import product5InBS from "./home/bestseller-products/product-5.jpeg";
+import product6InBS from "./home/bestseller-products/product-6.jpeg";
+import product7InBS from "./home/bestseller-products/product-7.jpeg";
+import product8InBS from "./home/bestseller-products/product-8.jpeg";
 
 /*Hero Bot IMGS*/
 import summerHeroBot from "./home/hero-Bot/heroBot-summer.png";
@@ -40,18 +40,33 @@ import category3 from "./product-list/categories/category-3.jpg";
 import category4 from "./product-list/categories/category-4.jpg";
 import category5 from "./product-list/categories/category-5.jpg";
 
+/*PRODUCTS*/
+import product1 from "./product-list/products/product-1.jpg";
+import product2 from "./product-list/products/product-2.jpg";
+import product3 from "./product-list/products/product-3.jpg";
+import product4 from "./product-list/products/product-4.jpg";
+import product5 from "./product-list/products/product-5.jpg";
+import product6 from "./product-list/products/product-6.jpg";
+import product7 from "./product-list/products/product-7.jpg";
+import product8 from "./product-list/products/product-8.jpg";
+import product9 from "./product-list/products/product-9.jpg";
+import product10 from "./product-list/products/product-10.jpg";
+import product11 from "./product-list/products/product-11.jpg";
+import product12 from "./product-list/products/product-12.jpg";
 
 export const Images = {
     home: {
         heroTop: { summerHeroTop, winterHeroTop },
         categories: { men, women, accessories, kids },
-        bestSellerProducts: { product1, product2, product3, product4, product5, product6, product7, product8, },
+        bestSellerProducts: { product1InBS, product2InBS, product3InBS, product4InBS, product5InBS, product6InBS, product7InBS, product8InBS, },
         heroBot: { summerHeroBot, winterHeroBot },
         popularProduct,
         featuredPosts: { post1, post2, post3 },
     },
     productList:{
-        categories:{category1,category2,category3,category4,category5}
+        categories:{category1,category2,category3,category4,category5},
+        products:{product1,product2,product3,product4,product5,product6,product7,product8,product9,product10,product11,product12}
     }
+
 
 }
