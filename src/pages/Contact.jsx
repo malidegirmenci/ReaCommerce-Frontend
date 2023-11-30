ceada880e868263142f28d833cb17cf4fd1b7d85
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-30 16:35:58 +0300

    text enlarged and button changed as rounded

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,12 +22,13 @@ export default function Contact() {
             <div className="relative h-screen bg-cover bg-right bg-no-repeat" style={{ backgroundImage: `url('${backgroundSrc}')` }}>
                 <div className="absolute z-10 inset-0 flex items-center w-[80%] mx-auto gap-20">
                     {isShowForm ? <ContactForm setShowForm={setShowForm} /> :
-                        <div className="flex flex-col items-center w-1/2">
-                            <h2 className="text-white text-4xl font-bold leading-[50px] tracking-tight mb-4">CONTACT US</h2>
-                            <p className="text-white text-sm font-normal leading-tight mb-6">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics</p>
-                            <button onClick={() => setShowForm(!isShowForm)} className="px-10 py-4 rounded-sm bg-[#23A6F0] text-white text-sm font-bold leading-snug tracking-tight">CONTACT US</button>
-                        </div>}
-                    <div className="flex flex-wrap gap-10 w-1/2">
+                        <div className="flex flex-col items-start w-1/2 ">
+                            <h2 className="text-white text-5xl font-bold leading-[50px] tracking-tight mb-4">CONTACT US</h2>
+                            <p className="text-white text-md font-normal leading-tight mb-6 w-[60%]">Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics</p>
+                            <button onClick={() => setShowForm(!isShowForm)} className="px-10 py-3 rounded-md bg-[#23A6F0] text-white text-sm font-bold leading-snug tracking-tight">CONTACT US</button>
+                        </div>
+                    }
+                    <div className="flex flex-wrap gap-10 w-1/2 ">
                         {locationsArr}
                     </div>
                 </div>
