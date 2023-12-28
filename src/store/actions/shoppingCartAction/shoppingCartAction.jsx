c27ddddc572ad95da68eb188d650c33a44f46153
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-29 02:27:58 +0300

    created actions for shopping cart

---
+++ src/store/actions/shoppingCartAction/shoppingCartAction.jsx
@@ -0,0 +1,32 @@
+import * as types from './shoppingCartActionTypes';
+import { instanceAxios } from '../../store';
+
+const addToCart = (product, count) => ({
+    type: types.ADD_TO_CART,
+    payload: {
+        count,
+        product
+    }
+});
+
+const removeFromCart = (productId) => ({
+    type: types.REMOVE_FROM_CART,
+    payload: productId
+});
+
+const updatePaymentInfo = (paymentInfo) => ({
+    type: types.UPDATE_PAYMENT_INFO,
+    payload: paymentInfo
+});
+
+const updateAddressInfo = (addressInfo) => ({
+    type: types.UPDATE_ADDRESS_INFO,
+    payload: addressInfo
+});
+
+export {
+    addToCart,
+    removeFromCart,
+    updatePaymentInfo,
+    updateAddressInfo
+};
