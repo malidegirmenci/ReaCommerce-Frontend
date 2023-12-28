dae55dbbe86625f8f7bedf30c8e00e66246dc89b
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-29 02:29:20 +0300

    imported shopping cart reducer

--- src/store/reducers/index.jsx
+++ src/store/reducers/index.jsx
@@ -2,13 +2,14 @@ import { combineReducers } from 'redux';
 import globalReducer from './globalReducer';
 import userReducer from './userReducer';
 import productReducer from './productReducer';
-/*import shoppingCartReducer from './shoppingCartReducer';
-import storeReducer from './storeReducer'; */
+import shoppingCartReducer from './shoppingCartReducer';
+//import storeReducer from './storeReducer'; 
 
 const rootReducer = combineReducers({
     global: globalReducer,
     user: userReducer,
     products:productReducer,
+    shoppingCart:shoppingCartReducer,
 });
 
 export default rootReducer;
