24ca2733aef5ea14dc821cd3519a5f44a227e678
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-29 02:36:09 +0300

    add store reducer in combine reducers

--- src/store/reducers/index.jsx
+++ src/store/reducers/index.jsx
@@ -3,13 +3,14 @@ import globalReducer from './globalReducer';
 import userReducer from './userReducer';
 import productReducer from './productReducer';
 import shoppingCartReducer from './shoppingCartReducer';
-//import storeReducer from './storeReducer'; 
+import storeReducer from './storeReducer';
 
 const rootReducer = combineReducers({
     global: globalReducer,
     user: userReducer,
-    products:productReducer,
-    shoppingCart:shoppingCartReducer,
+    products: productReducer,
+    shoppingCart: shoppingCartReducer,
+    store: storeReducer
 });
 
 export default rootReducer;
