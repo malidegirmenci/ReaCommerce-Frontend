a420b969ff251eb415d1ff255e3c9bceeda9797c
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-31 22:24:52 +0300

    categories linked

--- src/components/product-list/CategoryCard.jsx
+++ src/components/product-list/CategoryCard.jsx
@@ -1,13 +1,19 @@
-
-
-export default function  CategoryCard(props) {
-    const { title,rating, gender, img } = props.data;
-        return (
-        <div className="flex flex-col justify-center items-center bg-cover bg-center w-full h-80 relative cursor-pointer ease-out duration-300 hover:scale-105 hover:ease-out hover:duration-300" style={{ backgroundImage: `url('${img}')` }}>
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+export default function CategoryCard(props) {
+    const history = useHistory();
+    const { pathname, search } = useLocation();
+    const { title, rating, gender, img, code } = props.data;
+    return (
+        <Link
+            to={`/${gender === 'k' ? 'kadin' : 'erkek'}/${code.slice(2)}${search}`}
+            className="flex flex-col justify-center items-center bg-cover bg-center w-full h-80 relative cursor-pointer ease-out duration-300 hover:scale-105 hover:ease-out hover:duration-300"
+            style={{ backgroundImage: `url('${img}')` }}>
             <h6 className="text-white text-base font-bold leading-normal tracking-tight z-1">{gender == 'k' ? 'KADIN' : 'ERKEK'}</h6>
             <h6 className="text-white text-base font-bold leading-normal tracking-tight z-1">{title}</h6>
             <h6 className="text-white text-sm font-normal leading-tight tracking-tight z-1">{rating} rating</h6>
             <div className="bg-neutral-800 bg-opacity-25 absolute w-full h-full"></div>
-        </div>
+        </Link>
     )
 }
