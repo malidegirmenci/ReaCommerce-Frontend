e3461d0cba5ec74d40e80dab3f0c08bd08c81c1a
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-01 01:14:17 +0300

    rm category added description

--- src/components/general/ProductCard.jsx
+++ src/components/general/ProductCard.jsx
@@ -1,16 +1,17 @@
 export default function ProductCard(props) {
-    const { name, category, price, discountedPrice, colors, img } = props.data;
+    const { name, price, images, description } = props.data;
+    const priceWithoutDiscount = price+(price*0.3);
     return (
         <div className="flex flex-col text-center w-[20%] max-sm:w-full shadow-md rounded-b-md cursor-pointer ease-out duration-300 hover:scale-105 hover:ease-out hover:duration-300">
             <div className="h-4/5 ">
-                <img src={img} className="max-w-full max-sm:w-full h-full object-cover rounded-t-md  " />
+                <img src={images[0].url} className="max-w-full max-sm:w-full h-full object-cover rounded-t-md  " />
             </div>
             <div className="p-6 flex-col gap-2">
                 <h5 className="text-slate-800 text-base font-bold leading-normal tracking-tight">{name}</h5>
-                <h5 className="text-neutral-500 text-sm font-bold leading-normal tracking-tight">{category}</h5>
+                <h5 className="text-neutral-500 text-sm font-bold leading-normal tracking-tight truncate">{description}</h5> 
                 <div className="flex gap-1 justify-center">
-                    <h5 className="text-stone-300 text-base font-bold leading-normal tracking-tight">${price}</h5>
-                    <h5 className="text-teal-700 text-base font-bold leading-normal tracking-tight">${discountedPrice}</h5>
+                    <h5 className="text-stone-300 text-base font-bold leading-normal tracking-tight">${priceWithoutDiscount.toFixed(2)}</h5>
+                    <h5 className="text-teal-700 text-base font-bold leading-normal tracking-tight">${price}</h5>
                 </div>
                 <div className="w-20 h-4 justify-start items-center gap-1.5 inline-flex">
                     <div className="w-4 h-4 bg-sky-500 rounded-full" />
