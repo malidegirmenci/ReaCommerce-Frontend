1a6125ffcf3a1fe19f7bf987c26de643ebb04f49
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-01 01:12:36 +0300

    renamed key to CategoryCard

--- src/components/product-list/Categories.jsx
+++ src/components/product-list/Categories.jsx
@@ -2,8 +2,8 @@ import CategoryCard from "./CategoryCard";
 import { useSelector } from "react-redux";
 export default function Categories() {
     const categories = useSelector((store) => store.global.categories)
-    const categoriesField = categories.sort((a, b) => b.rating - a.rating).slice(0, 5).map(item => {
-        return <CategoryCard data={item} key={item.key} />
+    const categoriesField = categories.sort((a, b) => b.rating - a.rating).slice(0, 5).map((item,key) => {
+        return <CategoryCard data={item} key={key} />
     })
     return (
         <div className="flex w-full object-center max-sm:flex-col max-sm:gap-4">
