64d6f6d44567ac66108f8f37a79b632f2e341e87
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-01 20:06:24 +0300

    fetchTypes imported and removed pageCount and activePage and added error

--- src/store/reducers/productReducer.jsx
+++ src/store/reducers/productReducer.jsx
@@ -1,10 +1,11 @@
 import * as types from '../actions/productAction/productActionTypes';
+import * as fetchTypes from '../actions/fetchStatesTypes';
+
 const initialState = {
     productList: [],
     totalProductCount: 0,
-    pageCount: 0,
-    activePage: 1,
-    fetchState: 'NOT_FETCHED'
+    error:"",
+    fetchState: fetchTypes.NOT_FETCHED,
 };
 
 const productReducer = (state = initialState, action) => {
@@ -12,27 +13,25 @@ const productReducer = (state = initialState, action) => {
         case types.FETCH_PRODUCTS_REQUEST:
             return {
                 ...state,
-                fetchState: 'FETCHING'
+                fetchState: fetchTypes.FETCHING
             };
         case types.FETCH_PRODUCTS_SUCCESS:
             return {
                 ...state,
-                fetchState: 'FETCHED',
+                fetchState: fetchTypes.FETCHED,
                 productList: action.payload.productList,
                 totalProductCount: action.payload.totalProductCount,
             };
         case types.FETCH_PRODUCTS_FAILURE:
             return {
                 ...state,
-                fetchState: 'FAILED'
-            };
-        case types.CHANGE_ACTIVE_PAGE:
-            return {
-                ...state,
-                activePage: action.payload
+                fetchState: fetchTypes.FAILED,
+                error:action.payload
             };
         default:
             return state;
     }
+
+
 };
 export default productReducer;
