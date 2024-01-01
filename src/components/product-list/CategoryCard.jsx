e48cf8679dd102622e355b951bdbf0d889abe855
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-01 20:01:44 +0300

    changed  url path as shopping

--- src/components/product-list/CategoryCard.jsx
+++ src/components/product-list/CategoryCard.jsx
@@ -5,7 +5,7 @@ export default function CategoryCard(props) {
     const { id,title, rating, gender, img, code } = props.data;
     return (
         <Link
-            to={`/${gender === 'k' ? 'kadin' : 'erkek'}/${code.slice(2)}${search}`}
+            to={`/shopping/${gender === 'k' ? 'kadin' : 'erkek'}/${code.slice(2)}${search}`}
             className="flex flex-col justify-center items-center bg-cover bg-center w-full h-80 relative cursor-pointer ease-out duration-300 hover:scale-105 hover:ease-out hover:duration-300"
             style={{ backgroundImage: `url('${img}')` }} key={id}>
             <h6 className="text-white text-base font-bold leading-normal tracking-tight z-1">{gender == 'k' ? 'KADIN' : 'ERKEK'}</h6>
