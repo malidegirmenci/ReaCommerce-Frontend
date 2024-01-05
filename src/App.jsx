44e876fd081d2b5d32c636f5c55eda31d2e610bd
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-05 15:55:00 +0300

    fixed verify vulnerability in loggin and imported PrivateRoute for order page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
@@ -15,28 +15,29 @@ import Team from './pages/Team';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
-
-import useLocalStorage from './hooks/useLocalStorage';
+import Order from './pages/Order';
 import axiosWithAuth from './api/axiosWithAuth';
 
 import { updateCategories } from './store/actions/globalAction/globalAction';
 import { userSuccess } from './store/actions/userAction/userAction';
 import { fetchProducts } from './store/actions/productAction/productAction';
+import PrivateRoute from './components/general/PrivateRoute';
+
 
 function App() {
   const dispatch = useDispatch();
-
-  const user = useSelector((store) => store.user.response);
-
-  const [token, setToken] = useLocalStorage("Token", "");
-
+  const [isEqualToken, setIsEqualToken] = useState(false);
+  const user = useSelector((state) => state.user.response)
+  let isLoggedIn = user.hasOwnProperty("token")
+  
   useEffect(() => {
-    if (token) {
+    if (localStorage.getItem("Token")) {
       axiosWithAuth()
         .get("/verify")
         .then((response) => {
-          dispatch(userSuccess(response.data));
-          user.length && setToken(user.token);
+          dispatch(userSuccess(response.data))
+          localStorage.setItem("Token", response.data.token);
+          setIsEqualToken(true);
         })
         .catch((error) => {
           console.log("Error: ", error);
@@ -45,40 +46,44 @@ function App() {
     }
     dispatch(updateCategories());
     dispatch(fetchProducts());
-
-  }, []);
+  }, [isLoggedIn])
   return (
     <>
       <Header />
       <Switch>
-        <Route path="/login">
+        <Route path="/login" exact>
           <Login />
         </Route>
-        <Route path="/signup">
+        <Route path="/signup" exact>
           <SignUp />
         </Route>
-        <Route path="/team">
+        <Route path="/team" exact>
           <Team />
         </Route>
-        <Route path="/about">
+        <Route path="/about" exact>
           <About />
         </Route>
-        <Route path="/contact">
+        <Route path="/contact" exact>
           <Contact />
         </Route>
-        <Route path="/cart">
+        <PrivateRoute
+          path="/order"
+          component={Order}
+          isAuthenticated={isEqualToken}
+        />
+        <Route path="/cart" exact>
           <Cart />
         </Route>
-        <Route path="/shopping/:gender/:category/:productId/:productNameSlug">
+        <Route path="/shopping/:gender/:category/:productId/:productNameSlug" exact>
           <ProductPage />
         </Route>
-        <Route path="/shopping/:gender?/:category?">
+        <Route path="/shopping/:gender?/:category?" exact>
           <ProductList />
         </Route>
-        <Route path="/home">
+        <Route path="/home" exact>
           <Home />
         </Route>
-        <Route path="/">
+        <Route path="/" exact>
           <Home />
         </Route>
       </Switch>
