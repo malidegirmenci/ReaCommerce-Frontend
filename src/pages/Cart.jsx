ae564191ef992f29a64472c77761eb984ea559dd
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-06 17:07:50 +0300

    splitted summary field to summary.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,17 +8,15 @@ import {
     updateCartItemQuantity,
 } from "../store/actions/shoppingCartAction/shoppingCartAction";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleRight, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
-import { useState } from "react";
+import Summary from "../components/cart/Summary";
 
 function Cart() {
     const history = useHistory();
     const dispatch = useDispatch();
     const { cart } = useSelector((store) => store.shoppingCart);
     const categories = useSelector((store) => store.global.categories);
-    const couponCodeList = ["FREEDAY", "GIVEAWAY", "NOTTODAY"]
-    const [couponCode, setCouponCode] = useState('');
     let productTotal = cart
         .reduce((sum, inCart) => {
             return inCart.checked
@@ -30,11 +28,8 @@ function Cart() {
     let productCount = cart.reduce((sum, inCart) => {
         return inCart.checked ? sum + inCart.count : sum;
     }, 0);
-    const shippingPrice = 4.99
-    const freeShippingPriceLimit = 100;
     const amountLimit = 10;
-    const [toggleCouponTextArea, setToggleCouponTextArea] = useState(false);
-    const getProductURL = (productName,productId, categoryId,) => {
+    const getProductURL = (productName, productId, categoryId,) => {
         const catCode = categories.find(
             (c) => c.id == categoryId
         )?.code;
@@ -137,64 +132,7 @@ function Cart() {
                                 );
                             })}
                         </div>
-                        <div className="w-[30%] flex flex-col gap-4">
-                            <button
-                                className="bg-[#0ea5e9] ring-1  items-center rounded-md py-3 w-full flex gap-2 justify-center font-bold text-white"
-                                onClick={(e) => {
-                                    history.push("/order");
-                                }}
-                            >
-                                <span>Create Order</span>
-                                <FontAwesomeIcon icon={faAngleRight} className="text-white" size="lg" />
-                            </button>
-                            <div className="border border-solid border-neutral rounded-md p-5 flex flex-col gap-4">
-                                <h3 className="font-bold text-lg mb-4">Order Summary</h3>
-                                <div className="flex justify-between gap-2">
-                                    <p>Products:</p>
-                                    <p className="font-bold">${productTotal}</p>
-                                </div>
-                                <div className="flex justify-between gap-2">
-                                    <p>Shipping: </p>
-                                    <p className="font-bold">${shippingPrice}</p>
-                                </div>
-                                {productTotal >= freeShippingPriceLimit && (
-                                    <div className="flex justify-between gap-2">
-                                        <p>Free Shipping for orders ${freeShippingPriceLimit} or Above</p>
-                                        <p className="font-bold text-success">-${shippingPrice}</p>
-                                    </div>
-                                )}
-                                {couponCode &&
-                                    <div className="flex justify-between ">
-                                        <p> 10 percent discount applied</p>
-                                        <p className="font-bold text-success">-${(productTotal * 0.1).toFixed(2)}</p>
-                                    </div>}
-                                <hr />
-                                <div className="flex justify-between gap-2">
-                                    <p>Order Total: </p>
-                                    <p className="font-bold text-success">
-                                        ${productTotal >= freeShippingPriceLimit
-                                            ? productTotal - (couponCodeList.includes(couponCode) ? (productTotal * 0.1) : 0)
-                                            : (parseFloat(productTotal) + shippingPrice).toFixed(2)}
-                                    </p>
-                                </div>
-                            </div>
-                            <button className="ring-1 text-slate-700 rounded-md py-3 w-full flex gap-2 items-center justify-center ring-slate-400" onClick={() => setToggleCouponTextArea(!toggleCouponTextArea)}>
-                                <FontAwesomeIcon icon={faPlus} />
-                                <span>ENTER COUPON CODE</span>
-                            </button>
-                            <input id="coupon-code"
-                                onBlur={(e) => setCouponCode(e.target.value)}
-                                type="text" className={`py-3 px-2 ring-1 text-slate-700 rounded-md focus:outline-none w-full bg-white ring-slate-400 ease-in-out duration-100 ${toggleCouponTextArea ? '' : 'invisible'}`} placeholder="Enter your code" />
-                            <button
-                                className="bg-[#0ea5e9] ring-1 rounded-md py-3 w-full items-center flex gap-2 justify-center font-bold text-white"
-                                onClick={(e) => {
-                                    history.push("/order");
-                                }}
-                            >
-                                <span>Create Order</span>
-                                <FontAwesomeIcon icon={faAngleRight} className="text-white" size="lg" />
-                            </button>
-                        </div>
+                        <Summary/>
                     </div>
                 ) : (
                     <div className="px-4 text-error text-lg font-semibold">Your cart is empty.</div>
