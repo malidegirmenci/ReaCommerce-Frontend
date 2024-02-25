0b473be63497d95f05580a91f79143d31d2914e4
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-02-25 03:44:56 +0300

    removed unused import axios

--- src/store/actions/storeAction/storeAction.jsx
+++ src/store/actions/storeAction/storeAction.jsx
@@ -1,5 +1,4 @@
 import * as types from './storeActionTypes';
-import  instanceAxios  from '../../../api/axiosInstance';
 
 export const setSellerStore = (storeData) => ({
     type: types.SET_SELLER_STORE,
